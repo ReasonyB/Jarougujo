52fc000f9ffa7d7c1252c4b13468904775d415d0
ParkYiJun <ydlwnsj25@gmail.com>
2022-06-29 19:16:41 +0900

    Chapter07

---
+++ Chap07/ListBaseQueue.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "ListBaseQueue.h"
+
+void QueueInit(Queue * pq){
+	pq->front = NULL;
+	pq->rear = NULL;
+}
+
+int QIsEmpty(Queue *pq){
+	if(pq->front == NULL) return TRUE;
+	else return FALSE;
+}
+
+void Enqueue(Queue * pq, Data data){
+	Node * newNode = (Node *)malloc(sizeof(Node));
+	newNode->data = data;
+	newNode->next = NULL;
+	
+	if(QIsEmpty(pq)){
+		pq->front = newNode;
+		pq->rear = newNode;
+	}
+	else{
+		pq->rear->next = newNode;
+		pq->rear = newNode;
+	}
+}
+
+Data Dequeue(Queue * pq){
+	Node * delNode;
+	Data delData;
+	
+	if(QIsEmpty(pq)){
+		printf("Queue Is Empty!\n");
+		exit(-1);
+	}
+	
+	delNode = pq->front;
+	delData = delNode->data;
+
+	pq->front = pq->front->next;
+	free(delNode);
+	
+	return delData;
+}
+
+Data QPeek(Queue * pq){
+	if(QIsEmpty(pq)){
+		printf("Queue Is Empty!\n");
+		exit(-1);
+	}
+	return pq->front->data;
+}
